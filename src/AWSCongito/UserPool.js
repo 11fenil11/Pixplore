d0c3dc4db0d56c69ef75b627411a223826941d26
11fenil11 <it.fenil@gmail.com>
2022-08-08 19:39:10 -0300

    Changes
    Deleting previous version.

--- src/AWSCongito/UserPool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-const poolData = {
-    UserPoolId: "us-east-1_1ideWQ3uC",
-    ClientId: "7arop3faj2t6nu7n8pe9ij6bru"
-}
-
-export default new CognitoUserPool(poolData);
